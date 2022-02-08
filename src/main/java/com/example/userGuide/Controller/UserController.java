284064fb315022dfec5d04ac60e8ca89e1f4c6b0
Austin Lehman <45836106+austinlehman@users.noreply.github.com>
2022-02-07 22:03:10 -0600

    Add files via upload

---
+++ src/main/java/com/example/userGuide/Controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.userGuide.Controller;
+
+import com.example.userGuide.Service.UserService;
+import com.example.userGuide.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("user/")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("users")
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User UserById(@PathVariable("id") Long id) {
+        return userService.userById(id);
+    }
+
+    @PostMapping("post")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
+
+
